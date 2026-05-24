adfb34cb0af3db4bf279e40e90e0fdecf80d11f5
Avijit Pal <palavijit713130@gmail.com>
2026-05-24 21:34:42 +0530

    Fix Firebase auth

--- static/firebase-config.js
+++ static/firebase-config.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDFG8h0ssiK0j6FLN6ukfiJSk4WUT6350I",
+  apiKey: "AIzaSyDFG8h0ssjkOj6FLN6ukfiJSk4WUT6350I",
   authDomain: "fashion-store-b34e1.firebaseapp.com",
   projectId: "fashion-store-b34e1",
-  storageBucket: "fashion-store-b34e1.firebasestorage.app",
+  storageBucket: "fashion-store-b34e1.appspot.com",
   messagingSenderId: "958992471571",
-  appId: "1:958992471571:web:94ef3cdb929ec3a71151e6",
-  measurementId: "G-T56T1FM69V"
+  appId: "1:958992471571:web:94ef3cdb929ec3a71151e6"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+export { app };
